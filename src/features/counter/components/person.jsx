bf2c6aa52af6d95c1caff9561a6ee702cf27ae3f
Hasanmohammadi <hassanshm97@gmail.com>
2021-06-10 02:55:18 +0430

    change rank color

--- src/features/counter/components/person.jsx
+++ src/features/counter/components/person.jsx
@@ -1,10 +1,20 @@
 import style from "./style.module.css";
-const PersonInfo = ({name , index , point}) => {
+const PersonInfo = ({ name, index, point }) => {
   return (
     <div className={style.personContainer}>
-      <div className={style.rank}><p className={style.personP}>{index + 1}</p></div>
-      <div className={style.name}><p className={style.personName}>{name}</p></div>
-      <div className={style.point}><p className={style.personP}>{point}</p></div>
+      <div className={style.rank}>
+        {index + 1 === 1 ? (
+          <p  className={style.personP} style={{color:"goldenrod"}}>{index + 1}</p>
+        ) : (
+          <p className={style.personP}>{index + 1}</p>
+        )}
+      </div>
+      <div className={style.name}>
+        <p className={style.personName}>{name}</p>
+      </div>
+      <div className={style.point}>
+        <p className={style.personP}>{point}</p>
+      </div>
     </div>
   );
 };
